f1ad39a74710f2abf87430229a72e413e6afa4fb
Emil Örberg <32409967+spladder87@users.noreply.github.com>
2019-12-10 00:15:43 +0100

    init version
    added products class

---
+++ Models/ProductsContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bageriet.Models
+{
+    public class ProductsContext
+    {
+        public List<Product> Products { get; set; }
+    }
+}
